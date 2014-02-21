a44134c1ea38ad79ad744976e8146cc527806943
goruha <goruha@goruha-laptop.(none)>
2014-02-21 23:44:29 +0700

    add latest code

--- src/sources/code/html/custom.js
+++ src/sources/code/html/custom.js
@@ -1,13 +1,44 @@
-$(document).ready (function() {
-	$('#div-0').each(function() {
-	    $(this).click(function() {
-		$('#div-2').after($('<div class="devop">Cool</div>'));
-	    });
-	 });
+/*$(document).ready (function() {
+$('.devop').click(
+ 		function () {
+                         alert('Warning!!!');
 
-	
+});
+
+});*/
+
+$(document).ready (function() {
+	$('.devop').bind('click', function () {
+                         alert('Warning!!!');
+	});
+
+$('#select-all-select').click(
+	function () {
+		$('input.test').attr('checked', 'checked');
+	}
+);
+});
+
+
+ $(document).on('click', '.devop', function() {
+		alert('Warning!!!');	
+	    });
+
+$(document).ready (function() {
+
+	$('.leaf').each(
+		function() {
+	    		$(this).click(
+				function() {
+					$(this).after($('<div class="devop">Cool</div>'));
+	    			});
+		 });
 })
+
+/*
+
+
 	    $(document).on('click', '.devop', function() {
 		$(this).after($('<div class="devop">Cool 2</div>'));
 	    });
-
+*/
