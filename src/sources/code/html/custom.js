49d325ed01520e1bb4460fca444fcd360fe0c7a9
Igor Rodionov <goruha@gmail.com>
2014-03-01 22:40:11 +0700

    Html/css/js presentation

--- src/sources/code/html/custom.js
+++ src/sources/code/html/custom.js
@@ -1,44 +1,44 @@
 /*$(document).ready (function() {
-$('.devop').click(
- 		function () {
-                         alert('Warning!!!');
+ $('.devop').click(
+ function () {
+ alert('Warning!!!');
 
+ });
+
+ });*/
+
+$(document).ready(function () {
+  $('.devop').bind('click', function () {
+    alert('Warning!!!');
+  });
+
+  $('#select-all-select').click(
+    function () {
+      $('input.test').attr('checked', 'checked');
+    }
+  );
 });
 
-});*/
 
-$(document).ready (function() {
-	$('.devop').bind('click', function () {
-                         alert('Warning!!!');
-	});
-
-$('#select-all-select').click(
-	function () {
-		$('input.test').attr('checked', 'checked');
-	}
-);
+$(document).on('click', '.devop', function () {
+  alert('Warning!!!');
 });
 
+$(document).ready(function () {
 
- $(document).on('click', '.devop', function() {
-		alert('Warning!!!');	
-	    });
-
-$(document).ready (function() {
-
-	$('.leaf').each(
-		function() {
-	    		$(this).click(
-				function() {
-					$(this).after($('<div class="devop">Cool</div>'));
-	    			});
-		 });
+  $('.leaf').each(
+    function () {
+      $(this).click(
+        function () {
+          $(this).after($('<div class="devop">Cool</div>'));
+        });
+    });
 })
 
 /*
 
 
-	    $(document).on('click', '.devop', function() {
-		$(this).after($('<div class="devop">Cool 2</div>'));
-	    });
-*/
+ $(document).on('click', '.devop', function() {
+ $(this).after($('<div class="devop">Cool 2</div>'));
+ });
+ */
---
+++ src/sources/code/html/javascript_example.js
@@ -0,0 +1,5 @@
+var ol = document.getElementsByTagName('ol')[0]
+var hiter = ol.removeChild(ol.firstChild)
+var kovaren = ol.removeChild(ol.firstChild)
+ol.appendChild(kovaren)
+ol.appendChild(hiter)
